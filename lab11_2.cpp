448f115ad90470c3f7eedd24c44df0eced20eff9
tanyongrw <rudjaya22934@gmail.com>
2023-01-03 16:15:05 +0700

    finish

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,12 +6,16 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
+	string textline;
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+    dest << "-------------------- BOOM ---------------------\n";
+    while (getline(source,textline)){
+        dest << textline << '\n' ;
+    }
+    dest << "-------------------- HA!! ---------------------\n";
+    
     source.close();
     dest.close();
 	return 0;
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    double sum,sum1;
+    int N;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+    {
+    sum += atof(textline.c_str());
+    sum1 += pow(atof(textline.c_str()),2);
+    
+    N++;
+    }
+
+    cout << "Number of data = ";
+    cout << N << "\n";
+    cout << setprecision(3);
+    cout << "Mean = ";
+    cout << sum/N << "\n";
+    cout << "Standard deviation = ";
+    cout << sqrt(sum1/N-pow(sum/N,2));
+    
+}
